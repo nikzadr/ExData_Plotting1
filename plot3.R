7ba99945d43b3e2785ba0c17a635f8df92d78c3b
nikzadr <80527642+nikzadr@users.noreply.github.com>
2021-05-02 13:32:57 +0430

    added plot scripts

---
+++ plot3.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(lubridate)
+
+# beginning index: start of 2007-2-1 - 1
+i0 <- grep('1/2/2007', readLines('household_power_consumption.txt'))[1] - 1
+# ending index: start of 2007-2-3 - 1
+i1 <- grep('3/2/2007', readLines('household_power_consumption.txt'))[1] - 1
+
+# reading only the desired dates from the csv dataset while skipping the rest. 
+# variable names set according to description
+data <- tibble(read.csv('household_power_consumption.txt',
+                        header = FALSE,
+                        sep = ';', 
+                        skip = i0, 
+                        nrow = i1 - i0,
+                        col.names = c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')))
+
+# combining Date & Time columns into a DateTime column with DateTime class.
+data <- data %>% mutate(Date = dmy_hms(paste(data$Date, data$Time))) %>% select(-Time) %>% rename(DateTime = Date)
+
+## PLOT 3
+png(filename = 'plot3.png')
+plot(data$DateTime, data$Sub_metering_1, type = 'n', xlab = '', ylab = 'Evergy sub metering')
+points(data$DateTime, data$Sub_metering_1, type = 'l', col = 'black')
+points(data$DateTime, data$Sub_metering_2, type = 'l', col = 'red')
+points(data$DateTime, data$Sub_metering_3, type = 'l', col = 'blue')
+legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty = 1, lwd = 2, col = c('black', 'red', 'blue'))
+dev.off()
